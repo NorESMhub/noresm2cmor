6139b63b7d80cee962d41dc6c29e21d6ca70b5c2
IngoBethke <ingo.bethke@uni.no>
2019-09-05 09:23:48 +0200

    Added NorCPM1 cmor-ization of 20 deg thermocline depth.

--- scripts/create_CMIP6_maps.sh
+++ scripts/create_CMIP6_maps.sh
@@ -1,47 +1,55 @@
 #!/bin/sh -e 
 
 PROJECT=CMIP6
-ESGFROOT=/esg/gridftp_root/esg_dataroot/cmor/$PROJECT
+ESGFROOT=/esg/data/cmor/$PROJECT
 NIRDROOT=/projects/NS9034K/$PROJECT
 
-for MIP in `ls $NIRDROOT`
+for MIPTMP in `ls $NIRDROOT | sed 's/ /#/g'`
 do 
-	if [ $MIP == "cmorout" -o $MIP == "test" ] 
+        MIP="`echo $MIPTMP | sed 's/#/ /g'`"  
+	if [ "$MIP" == "cmorout" -o "$MIP" == "test" ] 
 	then
 		continue
 	fi
-	for INS in `ls $NIRDROOT/$MIP`
+	for INS in `ls "$NIRDROOT/$MIP"`
 	do
-		for MOD in `ls $NIRDROOT/$MIP/$INS`
+		for MOD in `ls "$NIRDROOT/$MIP/$INS"`
 		do
-			for EXP in `ls $NIRDROOT/$MIP/$INS/$MOD`
+			for EXP in `ls "$NIRDROOT/$MIP/$INS/$MOD"`
 			do
-				for RIP in `ls $NIRDROOT/$MIP/$INS/$MOD/$EXP`
+				for RIP in `ls "$NIRDROOT/$MIP/$INS/$MOD/$EXP"`
 				do
-					if [ ! -d $NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP} -o ! -e $NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP}.sha256sum ]
+					if [ ! -d "$NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP}" -o ! -e "$NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP}.sha256sum" ]
 					then 
 						continue
 					fi
-					MAPFILE=$NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP}.map
-					echo $MAPFILE
-					rm -f $MAPFILE
-					for ITEM in `sed 's/\ \ /%/g' $NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP}.sha256sum`
+					MAPFILE="$NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP}.map"
+					echo "$MAPFILE"
+                                        if [ -e "$MAPFILE" ] 
+                                        then 
+                                          echo "$MAPFILE exists; skipping..." 
+						continue
+					else      
+                                          echo "create new $MAPFILE"  
+                                        fi 
+					rm -f "$MAPFILE"
+					for ITEM in `sed 's/\ \ /%/g' "$NIRDROOT/$MIP/$INS/$MOD/$EXP/${RIP}.sha256sum"`
 					do
 						SHASUM=`echo $ITEM | cut -d"%" -f1`
                                                 RELPATH=`echo $ITEM | cut -d"%" -f2`
-						NIRDPATH=$NIRDROOT/$MIP/$INS/$MOD/$EXP/$RELPATH
-						ESGFPATH=$ESGFROOT/$MIP/$INS/$MOD/$EXP/$RELPATH
-						MTIME=`stat -Lc %Y $NIRDPATH`.0 
-						FSIZE=`stat -Lc %s $NIRDPATH`
+						NIRDPATH="$NIRDROOT/$MIP/$INS/$MOD/$EXP/$RELPATH"
+						ESGFPATH="$ESGFROOT/$MIP/$INS/$MOD/$EXP/$RELPATH"
+						MTIME=`stat -Lc %Y "$NIRDPATH"`.0 
+						FSIZE=`stat -Lc %s "$NIRDPATH"`
 						TABLE=`echo $RELPATH | cut -d'/' -f2`
 						FIELD=`echo $RELPATH | cut -d'/' -f3`
 						GRID=`echo $RELPATH | cut -d'/' -f4`
 						VERSION=`echo $RELPATH | cut -d'/' -f5 | cut -d'v' -f2`
                                                 DATASET="${PROJECT}.${MIP}.${INS}.${MOD}.${EXP}.${RIP}.${TABLE}.${FIELD}.${GRID}#${VERSION}"
  						MAPENTRY="$DATASET | $ESGFPATH | $FSIZE | mod_time=$MTIME | checksum=$SHASUM | checksum_type=SHA256"
-						echo $MAPENTRY >> $MAPFILE
+						echo "$MAPENTRY" >> "$MAPFILE"
 					done
-					chmod g+w $MAPFILE
+					chmod g+w "$MAPFILE"
 				done
 			done
 		done 
