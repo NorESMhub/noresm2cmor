50ac0951ea743b8e175f3a7fc8da393d805890d7
yanchunhe <yanchun.he@nersc.no>
2019-10-25 22:20:18 +0200

    Added functionality to scripts to create links and sha256sum for additional fields,even with the same data version tag

--- scripts/create_CMIP6_links.sh
+++ scripts/create_CMIP6_links.sh
@@ -5,17 +5,43 @@ pid=$$
 ROOT=/projects/NS9034K/CMIP6
 cd $ROOT
 
+# set data version
 #VER=v20190815
 #VER=v20190909
 #VER=v20190917
 VER=v20190920
+#VER=v20191009
+#VER=v20191018
+#VER=v20191022
 
+# set extra find operator
+fop="-mtime -2"     #modified in two days
+
+# set paths of cmorized data
 #folders+=(.cmorout/NorESM2-LM/1pctCO2/${VER})
 #folders+=(.cmorout/NorESM2-LM/abrupt-4xCO2/${VER})
 #folders+=(.cmorout/NorESM2-LM/hist-GHG/${VER})
 #folders+=(.cmorout/NorESM2-LM/historical/${VER})
 #folders+=(.cmorout/NorESM2-LM/hist-piAer/${VER})
 #folders+=(.cmorout/NorESM2-LM/hist-piNTCF/${VER})
+#folders+=(.cmorout/NorESM2-LM/piControl/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-spAer-aer/${VER})
+#folders+=(.cmorout/NorESM2-LM/esm-hist/${VER})
+#folders+=(.cmorout/NorESM2-LM/omip1/${VER})
+#folders+=(.cmorout/NorESM2-LM/omip2/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-spAer-anthro/${VER})
+
+folders+=(.cmorout/NorESM2-LM/1pctCO2-cdr/${VER})
+
+#folders+=(.cmorout/NorESM2-LM/pdSST-pdSIC/${VER})
+#folders+=(.cmorout/NorESM2-LM/pdSST-futArcSIC/${VER})
+#folders+=(.cmorout/NorESM2-LM/pdSST-piAntSIC/${VER})
+#folders+=(.cmorout/NorESM2-LM/pdSST-futAntSIC/${VER})
+
+#folders+=(.cmorout/NorESM2-LM/histSST/${VER})
+#folders+=(.cmorout/NorESM2-LM/histSST-piAer/${VER})
+#folders+=(.cmorout/NorESM2-LM/histSST-piNTCF/${VER})
+
 #folders+=(.cmorout/NorESM2-LM/piClim-4xCO2/${VER})
 #folders+=(.cmorout/NorESM2-LM/piClim-aer/${VER})
 #folders+=(.cmorout/NorESM2-LM/piClim-BC/${VER})
@@ -24,18 +50,25 @@ VER=v20190920
 #folders+=(.cmorout/NorESM2-LM/piClim-lu/${VER})
 #folders+=(.cmorout/NorESM2-LM/piClim-OC/${VER})
 #folders+=(.cmorout/NorESM2-LM/piClim-SO2/${VER})
-#folders+=(.cmorout/NorESM2-LM/piControl/${VER})
-#folders+=(.cmorout/NorESM2-LM/pdSST-pdSIC/${VER})
-#folders+=(.cmorout/NorESM2-LM/pdSST-futArcSIC/${VER})
-#folders+=(.cmorout/NorESM2-LM/piClim-spAer-aer/${VER})
-folders+=(.cmorout/NorESM2-LM/esm-hist/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-anthro/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-2xss/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-2xdust/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-2xDMS/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-2xVOC/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-CH4/${VER})
+#folders+=(.cmorout/NorESM2-LM/piClim-N2O/${VER})
+
+#folders+=(.cmorout/NorESM2-LM/piClim-histall/${VER})
 
 insitute=NCC
 for (( i = 0; i < ${#folders[*]}; i++ )); do
     folder=${folders[i]}
     echo "$folder"
 
-    find $folder -name *.nc -print 1>/tmp/flist.txt.$pid
+    # extra operatore, e.g., -mtime -2, modified within 2 days
+    find $folder $fop -name *.nc -print 1>/tmp/flist.txt.$pid
+
+    # if no files found
     if [ $? -ne 0 ]
     then
         continue
--- scripts/create_CMIP6_sha256sum.sh
+++ scripts/create_CMIP6_sha256sum.sh
@@ -1,145 +1,132 @@
 #!/bin/bash
-#set -ex
+#set -x
 
-if [ $# -gt 0 ] && [ $1 == "--help" ] 
-then
-    printf "Usage:\n"
-    printf "create_CMIP6_sha256sum.sh -e [path_to_experiment] -v [data_version] -O [TRUE]\n"
-    printf "\t Command line options:\n"
-    printf "\t -e, --exp=path_to_experiment     :specify path to the data [optional];\n"
-    printf "\t -v, --version=vyyyymmdd          :speicify variable version [optional];\n"
-    printf "\t -O, --overwrite=TRUE             :specify if overwrite existed sha256sum file [optional];\n"
-    exit 1
-else
-    while test $# -gt 0; do
-        case "$1" in
-            -e )
-                shift
-                if test $# -gt 0; then
-                    folders=($1)
-                else
-                    echo "ERROR: no experiment path specified (-e, --exp=)"
-                    echo "*** EXITING THE SCRIPT"
-                    exit 1
-                fi
-                shift
-                ;;
-            --exp=* )
-                folders=($(echo $1|sed -e 's/^[^=]*=//g'))
-                shift
-                ;;
-            -v )
-                shift
-                if test $# -gt 0; then
-                    version=$(echo $1 | sed 's/,/ /g')
-                else
-                    echo "ERROR: no version specified (-v, --version=)"
-                    echo "*** EXITING THE SCRIPT"
-                    exit 1
-                fi
-                shift
-                ;;
-            --version=* )
-                version=$(echo $1|sed -e 's/^[^=]*=//g'|sed 's/,/ /g')
-                shift
-                ;;
-            -O )
-                shift
-                if test $# -gt 0; then
-                    OverWrite=$(echo $1 | sed 's/,/ /g')
-                else
-                    echo "ERROR: no overwrite flag specified (-O, --overwrite=TRUE)"
-                    echo "*** EXITING THE SCRIPT"
-                    exit 1
-                fi
-                shift
-                ;;
-            --overwrite=* )
-                OverWrite=($(echo $1|sed -e 's/^[^=]*=//g'|sed 's/,/ /g'))
-                shift
-                ;;
-            * )
-                echo "ERROR: option $1 not allowed."
+# Data Version
+#Version=v20190815
+#Version=v20190909
+#Version=v20190917
+Version=v20190920
+#Version=v20191009
+#Version=v20191018
+#Version=v20191022
 
-                echo "*** EXITING THE SCRIPT"
-                exit 1
-                ;;
-        esac
-    done
-fi
-
-if [ -z "${folders[*]}" ]
-then
-    #folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/hist-piAer)
-    #folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/hist-piNTCF)
-    #folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-BC)
-    #folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-OC)
-    #folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-SO2)
-    #folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/1pctCO2)
-    #folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/abrupt-4xCO2)
-    #folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/historical)
-    #folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/piControl)
-    #folders+=(/projects/NS9034K/CMIP6/DAMIP/NCC/NorESM2-LM/hist-GHG)
-    #folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-4xCO2)
-    #folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-ghg)
-    #folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-lu)
-    #folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-aer)
-    #folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-control)
-    #folders+=(/projects/NS9034K/CMIP6/PAMIP/NCC/NorESM2-LM/pdSST-pdSIC)
-    #folders+=(/projects/NS9034K/CMIP6/PAMIP/NCC/NorESM2-LM/pdSST-futArcSIC)
-    #folders+=(/projects/NS9034K/CMIP6/OMIP/NCC/NorESM2-LM/omip1)
-    #folders+=(/projects/NS9034K/CMIP6/OMIP/NCC/NorESM2-LM/omip2)
-    #folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-spAer-aer)
-    #folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/esm-hist)
-    folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/esm-piControl)
-fi
-if [ -z "$version" ]
-then
-    #Version=v20190815
-    #Version=v20190909
-    #Version=v20190917
-    Version=v20190920
-fi
+# set if overwrite the whole .sha256sum file
 #OverWrite=TRUE
+
+# set if replace or skip the existing sha256sum entries
+WriteMode="skip"
+#WriteMode="replace"
+
+# AerChemMIP
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/hist-piAer)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/hist-piNTCF)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-BC)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-OC)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-SO2)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-2xss)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-2xdust)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-2xDMS)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-2xVOC)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-CH4)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/piClim-N2O)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/histSST)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/histSST-piAer)
+#folders+=(/projects/NS9034K/CMIP6/AerChemMIP/NCC/NorESM2-LM/histSST-piNTCF)
+
+# CMIP
+#folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/1pctCO2)
+#folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/abrupt-4xCO2)
+#folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/historical)
+#folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/piControl)
+#folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/esm-hist)
+#folders+=(/projects/NS9034K/CMIP6/CMIP/NCC/NorESM2-LM/esm-piControl)
+
+# DAMIP
+#folders+=(/projects/NS9034K/CMIP6/DAMIP/NCC/NorESM2-LM/hist-GHG)
+
+# RFMIP
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-4xCO2)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-ghg)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-lu)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-aer)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-control)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-spAer-aer)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-spAer-anthro)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-anthro)
+#folders+=(/projects/NS9034K/CMIP6/RFMIP/NCC/NorESM2-LM/piClim-histall)
+
+# PAMIP
+#folders+=(/projects/NS9034K/CMIP6/PAMIP/NCC/NorESM2-LM/pdSST-pdSIC)
+#folders+=(/projects/NS9034K/CMIP6/PAMIP/NCC/NorESM2-LM/pdSST-futArcSIC)
+#folders+=(/projects/NS9034K/CMIP6/PAMIP/NCC/NorESM2-LM/pdSST-piAntSIC)
+#folders+=(/projects/NS9034K/CMIP6/PAMIP/NCC/NorESM2-LM/pdSST-futAntSIC)
+
+# OMIP
+ #folders+=(/projects/NS9034K/CMIP6/OMIP/NCC/NorESM2-LM/omip1)
+ #folders+=(/projects/NS9034K/CMIP6/OMIP/NCC/NorESM2-LM/omip2)
+
+# CDRMIP
+folders+=(/projects/NS9034K/CMIP6/CDRMIP/NCC/NorESM2-LM/1pctCO2-cdr)
+
 
 pid=$$
 for (( i = 0; i < ${#folders[*]}; i++ )); do
     folder=${folders[i]}
     cd "$folder"
-    reals=($(find -maxdepth 1  -type d -name 'r*i*p*f*' -print |awk -F"/" '{print $NF}' ))
+    reals=($(find -maxdepth 1  -type d -name 'r*i*p*f*' -print |awk -F"/" '{print $NF}'|sort --version-sort ))
+    #reals=(r2i1p1f1)
+    #reals=(r3i1p1f1)
     for (( j = 0; j < ${#reals[*]}; j++ )); do
         real=${reals[j]}
         echo "Process $folder/$real"
-        if [ ! -z $OverWrite ] && [ "$OverWrite" == "TRUE" ]
-        then
-            rm -f ./.${real}.sha256sum
-        fi
-        k=1
         find $real/*/*/*/$Version -name *.nc -print 2>/dev/null 1>/tmp/flist.txt.$pid
         if [ $? -ne 0 ]
         then
-            echo "ERROR: no file found under $folder/$real/*/*/*/$Version"
-            echo "EXIT..."
-            exit 1
+            echo "WARNING: no file found under $folder/$real/*/*/*/$Version"
+            echo "SKIP $real ..."
+            continue
         fi
+
+        if [ ! -z $OverWrite ] && [ "$OverWrite" == "TRUE" ]
+        then
+            rm -f ./.${real}.sha256sum_${Version}
+        fi
+        cp /tmp/flist.txt.$pid /tmp/flist2.txt.$pid
+        touch .${real}.sha256sum_${Version}
         while read -r fname
         do
             bname=$(basename $fname)
-            grep "${Version}\/${bname}" .${real}.sha256sum >/dev/null 2>&1
+            grep "${Version}\/${bname}" .${real}.sha256sum_${Version} >/dev/null 2>&1
             if [ $? -eq 0 ]
             then
-                echo "replace ${Version}\/${bname}"
-                sed -i "/${Version}\/${bname}/d" .${real}.sha256sum
+                if [ $WriteMode == "replace" ]
+                then
+                    echo "replace ${Version}\/${bname}"
+                    sed -i "/${Version}\/${bname}/d" .${real}.sha256sum_${Version}
+                elif [ $WriteMode == "skip" ]
+                then
+                    echo "skip ${Version}\/${bname}"
+                    sed -i "/$bname/d" /tmp/flist2.txt.$pid
+                else
+                    echo "ERROR: unknow WriteMode: $WriteMode"
+                fi
             fi
-            sha256sum $fname &>>.${real}.sha256sum &
-            while [ $k -gt 15 ]; do
-                wait
-                let k=1
-            done
-            let k+=1
         done </tmp/flist.txt.$pid
-        rm -f /tmp/flist.txt.$pid
+
+        k=1
+        nf=$(cat /tmp/flist2.txt.$pid |wc -l)
+        while read -r fname
+        do
+            bname=$(basename $fname)
+            sha256sum $fname >>.${real}.sha256sum_${Version} &
+            echo -ne "sha256sum: $k/$nf files\r"
+            if [ $(($k%15)) -eq 0 ]; then
+                wait
+            fi
+            let k+=1
+        done </tmp/flist2.txt.$pid
+        rm -f /tmp/flist.txt.$pid /tmp/flist2.txt.$pid
     done
 done
-echo "ALL DONE"
+echo -e "\rALL DONE"
 
