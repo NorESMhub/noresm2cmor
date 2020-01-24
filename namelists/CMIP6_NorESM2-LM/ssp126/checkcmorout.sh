aebbc59e084645bc04e527abef51eabe8e45b108
yanchunhe <yanchun.he@nersc.no>
2020-01-24 08:07:26 +0100

    Add/update namelist for NorESM2-LM ssp126

--- namelists/CMIP6_NorESM2-LM/ssp126/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/ssp126/checkcmorout.sh
@@ -7,4 +7,4 @@ years1=(2015 $(seq 2021 10 2091))
 years2=(2020 $(seq 2030 10 2100))
 
 ${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -yrs1="${years1[*]}" -yrs2="${years2[*]}"
-
+#
