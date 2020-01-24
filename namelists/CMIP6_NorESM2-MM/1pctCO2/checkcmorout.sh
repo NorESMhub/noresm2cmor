9dc06f933663260c876e88535d44c18fac8ca980
yanchunhe <yanchun.he@nersc.no>
2020-01-24 08:31:57 +0100

    Add/update namelist for NorESM2-MM ssp126 ssp585 1pctCO2

--- namelists/CMIP6_NorESM2-MM/1pctCO2/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-MM/1pctCO2/checkcmorout.sh
@@ -3,8 +3,8 @@
 version=v20191108
 expid=1pctCO2
 model=NorESM2-MM
-years1=(1  $(seq 11 10 111))
-years2=(10 $(seq 20 10 120))
+years1=(0 1  $(seq 11 10 111))
+years2=(10 10 $(seq 20 10 120))
 years1+=($(seq 121 10 141))
 years2+=($(seq 130 10 150))
 
