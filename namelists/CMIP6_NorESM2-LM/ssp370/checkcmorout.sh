8d1f50d99a492578550e89fd3b017e1edd0b0f20
yanchunhe <yanchun.he@nersc.no>
2020-01-24 08:08:55 +0100

    Add/update namelist for NorESM2-LM ssp370

--- namelists/CMIP6_NorESM2-LM/ssp370/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/ssp370/checkcmorout.sh
@@ -3,8 +3,10 @@
 version=v20191108
 expid=ssp370
 model=NorESM2-LM
-years1=(2015 $(seq 2021 10 2091))
-years2=(2020 $(seq 2030 10 2100))
+years1=(2015 $(seq 2021 10 2041))
+years2=(2020 $(seq 2030 10 2050))
+years1+=(2051 $(seq 2051 10 2091))
+years2+=(2054 $(seq 2060 10 2100))
 
 ${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -yrs1="${years1[*]}" -yrs2="${years2[*]}"
 
