9218cce50494dd1362b990f0dfa107609aadb63f
yanchunhe <yanchun.he@nersc.no>
2020-07-14 15:07:30 +0200

    small fix to checkcmorout year segments
    esm-ssp585-ssp126Lu ssp370-ssp126Lu

--- namelists/CMIP6_NorESM2-LM/esm-ssp585-ssp126Lu/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/esm-ssp585-ssp126Lu/checkcmorout.sh
@@ -3,8 +3,8 @@
 version=v20200702
 expid=esm-ssp585-ssp126Lu
 model=NorESM2-LM
-years1=(2015 $(seq 2021 10 2091))
-years2=(2020 $(seq 2030 10 2100))
+years1=(2015 $(seq 2021 10 2081) 2091)
+years2=(2020 $(seq 2030 10 2090) 2099)
 
 ${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -yrs1="${years1[*]}" -yrs2="${years2[*]}"
 
--- namelists/CMIP6_NorESM2-LM/ssp370-ssp126Lu/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/ssp370-ssp126Lu/checkcmorout.sh
@@ -3,8 +3,8 @@
 version=v20200702
 expid=ssp370-ssp126Lu
 model=NorESM2-LM
-years1=(2015 $(seq 2021 10 2091))
-years2=(2020 $(seq 2030 10 2099))
+years1=(2015 $(seq 2021 10 2081) 2091)
+years2=(2020 $(seq 2030 10 2090) 2099)
 
 ${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -yrs1="${years1[*]}" -yrs2="${years2[*]}"
 
