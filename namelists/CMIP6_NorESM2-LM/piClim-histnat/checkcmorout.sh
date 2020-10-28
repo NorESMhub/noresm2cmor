7bd40255844cb616a70345b0bf6ec6611120bfa5
He Yanchun <yanchun.ho@gmail.com>
2020-10-28 12:57:36 +0100

    cron: update CMIP6_CV -- 2020-10-28T12:57

--- namelists/CMIP6_NorESM2-LM/piClim-histnat/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/piClim-histnat/checkcmorout.sh
@@ -3,12 +3,15 @@
 #version=v20191108b
 version=v20200218
 version=v20200702
+version=v20201001
 expid=piClim-histnat
 model=NorESM2-LM
 years1=(1849 $(seq 1850 10 1940))
 years2=(1859 $(seq 1859 10 1949))
 years1+=($(seq 1950 10 2000) 2010)
 years2+=($(seq 1959 10 2009) 2014)
+years1+=(2015 $(seq 2021 10 2091))
+years2+=(2020 $(seq 2030 10 2100))
 
 ${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -yrs1="${years1[*]}" -yrs2="${years2[*]}"
 
