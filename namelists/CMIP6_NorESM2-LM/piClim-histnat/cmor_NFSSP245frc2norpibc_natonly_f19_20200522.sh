981864c273f5409b59e38938b817ce8fea084513
He Yanchun <yanchun.ho@gmail.com>
2020-10-28 15:49:20 +0100

    update namelists for piClim-hist* experiments
    piClim-histnat piClim-histghg piClim-histall piClim-histaer

---
+++ namelists/CMIP6_NorESM2-LM/piClim-histnat/cmor_NFSSP245frc2norpibc_natonly_f19_20200522.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+
+# set active
+login0=true
+
+# initialize
+version=v20201001
+expid=piClim-histnat
+model=NorESM2-LM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# part 1
+#----------------
+CaseName=NFSSP245frc2norpibc_natonly_f19_20200522
+real=1
+physics=2
+years1=(2015 $(seq 2021 10 2091))
+years2=(2020 $(seq 2030 10 2100))
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -p=$physics -yrs1="${years1[*]}" -yrs2="${years2[*]}" -mpi=DMPI
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
+# PrePARE QC check, create links and update sha256sum
+${CMOR_ROOT}/workflow/cmorPost.sh -m=${model} -e=${expid} -v=${version} --verbose=false
