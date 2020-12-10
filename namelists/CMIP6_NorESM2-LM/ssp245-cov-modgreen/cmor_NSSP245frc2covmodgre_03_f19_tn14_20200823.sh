88007755cc2e70669c1f6e31bf7879d969a09166
He Yanchun <yanchun.ho@gmail.com>
2020-12-10 08:55:19 +0100

    Add/update recipes for covid scenarios
    including ssp245 ssp245-covid ssp245-cov-strgreen ssp245-cov-modgreen ssp245-cov-fossil

---
+++ namelists/CMIP6_NorESM2-LM/ssp245-cov-modgreen/cmor_NSSP245frc2covmodgre_03_f19_tn14_20200823.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+
+# set active
+login0=true
+
+# initialize
+version=v20201001
+expid=ssp245-cov-modgreen
+model=NorESM2-LM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# part 1
+#----------------
+CaseName=NSSP245frc2covmodgre_03_f19_tn14_20200823
+real=3
+physics=1
+forcing=2
+init=1
+years1=(2015 $(seq 2021 10 2041))
+years2=(2020 $(seq 2030 10 2050))
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -p=$physics -f=$forcing -i=$init -yrs1="${years1[*]}" -yrs2="${years2[*]}" -s=NS2345K -mpi=DMPI
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
+# PrePARE QC check, create links and update sha256sum
+#${CMOR_ROOT}/workflow/cmorPost.sh -m=${model} -e=${expid} -v=${version} --verbose=false
--- namelists/CMIP6_NorESM2-LM/ssp245-cov-strgreen/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/ssp245-cov-strgreen/checkcmorout.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#version=v20191108
-version=v20200218
 version=v20201001
-expid=ssp245
+expid=ssp245-cov-strgreen
 model=NorESM2-LM
-years1=(2015 $(seq 2021 10 2091))
-years2=(2020 $(seq 2030 10 2100))
+years1=(2015 $(seq 2021 10 2041))
+years2=(2020 $(seq 2030 10 2050))
 
 ${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -yrs1="${years1[*]}" -yrs2="${years2[*]}"
