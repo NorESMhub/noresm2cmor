61038a70c0c72d158ade94035c9e28c273a62838
He Yanchun <yanchun.ho@gmail.com>
2021-03-01 09:43:42 +0100

    cmorize NorESM2-LM histSST* physics 2

---
+++ namelists/CMIP6_NorESM2-LM/histSST/cmor_NFHISTnorbc_f19_20191107.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+
+# set active
+login0=true
+
+# initialize
+version=v20210218
+expid=histSST
+model=NorESM2-LM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# part 1
+#----------------
+CaseName=NFHISTnorbc_f19_20191107
+real=1
+physics=2
+forcing=1
+init=1
+years1=(1849 $(seq 1850 10 2000) 2010)
+years2=(1859 $(seq 1859 10 2009) 2014)
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -p=$physics -f=$forcing -i=$init -yrs1="${years1[*]}" -yrs2="${years2[*]}" -mpi=DMPI
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
+# PrePARE QC check, create links and update sha256sum
+${CMOR_ROOT}/workflow/cmorPost.sh -m=${model} -e=${expid} -v=${version} --verbose=false
