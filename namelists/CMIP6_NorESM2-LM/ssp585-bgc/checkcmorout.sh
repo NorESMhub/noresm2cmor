76db319400cb6cf61c70ed2621fdde48a36e6823
He Yanchun <yanchun.ho@gmail.com>
2021-08-25 14:49:09 +0200

    add/update namelists for C4MIP experiments
    * ssp534-over #291;
    * ssp534-over-bgc #292;
    * hist-bgc #293;
    * ssp585-bgc #294

---
+++ namelists/CMIP6_NorESM2-LM/ssp585-bgc/checkcmorout.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CMOR_ROOT=$(cd $(dirname $0) && cd ../../.. && pwd)
+# parse input parameters
+source $CMOR_ROOT/workflow/cmorParse.sh
+
+years1=(2015 $(seq 2020 10 2090) 2100)
+years2=(2019 $(seq 2029 10 2099) 2100)
+
+${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -o=$cmorout -yrs1="${years1[*]}" -yrs2="${years2[*]}"
+
