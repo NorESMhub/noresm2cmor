8d3fefe69e2f0bcbadb45f800dcf320613d536d2
He Yanchun <yanchun.ho@gmail.com>
2023-09-25 11:20:54 +0200

    cmorized NorESM2-LM experiments with new physics
    * 1pctCO2 (#341)
    * ssp245 (#344)
    * amip (#342)
    * amip-p4K (#343)

--- namelists/CMIP6_NorESM2-LM/ssp245/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/ssp245/checkcmorout.sh
@@ -4,8 +4,8 @@ CMOR_ROOT=$(cd $(dirname $0) && cd ../../.. && pwd)
 # parse input parameters
 source $CMOR_ROOT/workflow/cmorParse.sh
 
-years1=(2015 $(seq 2021 10 2091))
-years2=(2020 $(seq 2030 10 2100))
+years1=(2015 $(seq 2020 10 2090) 2100)
+years2=(2019 $(seq 2029 10 2099) 2100)
 
-${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -yrs1="${years1[*]}" -yrs2="${years2[*]}"
+${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -o=$cmorout -yrs1="${years1[*]}" -yrs2="${years2[*]}"
 
---
+++ namelists/CMIP6_NorESM2-LM/ssp245/cmor_NSSP245_f19_tn14_Forces_230609.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+CMOR_ROOT=$(cd $(dirname $0) && cd ../../.. && pwd)
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+login0=false
+
+# set active
+#login0=true
+login1=true
+
+# initialize
+version=v20230616
+expid=ssp245
+model=NorESM2-LM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# part 1
+#----------------
+CaseName=NSSP245_f19_tn14_Forces_230609
+real=1
+physics=4
+forcing=1
+init=1
+years1=(2015 $(seq 2020 10 2090) 2100)
+years2=(2019 $(seq 2029 10 2099) 2100)
+years1=(2060)
+years2=(2069)
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -p=$physics -f=$forcing -i=$init -yrs1="${years1[*]}" -yrs2="${years2[*]}" -mpi=DMPI
+#---
+fi
+#---
+
+if $login1
+then
+#----------------
+# part 2
+#----------------
+CaseName=NSSP245_f19_tn14_Forces_230609
+real=1
+physics=4
+forcing=1
+init=1
+years1=(2060)
+years2=(2069)
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -p=$physics -f=$forcing -i=$init -yrs1="${years1[*]}" -yrs2="${years2[*]}" -s=2 -mpi=DMPI
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
+# PrePARE QC check, create links and update sha256sum
+${CMOR_ROOT}/workflow/cmorPost.sh -m=${model} -e=${expid} -v=${version} --verbose=false
