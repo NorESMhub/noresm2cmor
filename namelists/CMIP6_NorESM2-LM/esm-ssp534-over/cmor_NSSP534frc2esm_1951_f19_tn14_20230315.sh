cfe798d088cb2623dbe3c0d37bb7a8001a545ce9
He Yanchun <yanchun.ho@gmail.com>
2023-10-03 10:35:49 +0200

    cmorized esm-ssp534-over (#346)

---
+++ namelists/CMIP6_NorESM2-LM/esm-ssp534-over/cmor_NSSP534frc2esm_1951_f19_tn14_20230315.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+CMOR_ROOT=$(cd $(dirname $0) && cd ../../.. && pwd)
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+
+# set active
+login0=true
+
+# initialize
+version=v20230616
+expid=esm-ssp534-over
+model=NorESM2-LM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# part 1
+#----------------
+CaseName=NSSP534frc2esm_1951_f19_tn14_20230315
+real=3
+physics=1
+forcing=1
+init=1
+years1=(2015 $(seq 2020 10 2090) 2100)
+years2=(2019 $(seq 2029 10 2099) 2100)
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -p=$physics -f=$forcing -i=$init -yrs1="${years1[*]}" -yrs2="${years2[*]}" -mpi=DMPI
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
+# PrePARE QC check, create links and update sha256sum
+#${CMOR_ROOT}/workflow/cmorPost.sh -m=${model} -e=${expid} -v=${version} --verbose=false
