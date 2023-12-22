9c4c9d99fe5a7c5a9497251c20f4416cab1302b1
He Yanchun <yanchun.ho@gmail.com>
2023-12-22 10:54:40 +0100

    recipes for esm-ssp534-over physics 2
    github issue #349

--- namelists/CMIP6_NorESM2-LM/esm-ssp534-over/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/esm-ssp534-over/checkcmorout.sh
@@ -6,8 +6,8 @@ source $CMOR_ROOT/workflow/cmorParse.sh
 
 years1=(2015 $(seq 2020 10 2090) 2100)
 years2=(2019 $(seq 2029 10 2099) 2100)
-#years1+=(2101 $(seq 2110 10 2280) 2290)
-#years2+=(2109 $(seq 2119 10 2289) 2299)
+years1+=(2101 $(seq 2110 10 2280) 2290)
+years2+=(2109 $(seq 2119 10 2289) 2299)
 
 ${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -o=$cmorout -yrs1="${years1[*]}" -yrs2="${years2[*]}"
 
