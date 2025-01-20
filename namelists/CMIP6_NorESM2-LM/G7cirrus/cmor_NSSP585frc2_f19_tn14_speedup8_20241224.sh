c89021fcbe2662005668ba000d2db42245722935
mvdebolskiy <mdeb@login2-nird-lmd.nird.login.svc.cluster.local>
2025-01-20 11:01:03 +0100

    Added G7cirrus GEOMIP experiment

---
+++ namelists/CMIP6_NorESM2-LM/G7cirrus/cmor_NSSP585frc2_f19_tn14_speedup8_20241224.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+CMOR_ROOT=$(cd $(dirname $0) && cd ../../.. && pwd)
+source ${CMOR_ROOT}/workflow/cmorRun1memb.sh
+
+# initialize
+login0=false
+
+# set active
+login0=true
+
+# initialize
+version=v20250115
+expid=G7cirrus
+model=NorESM2-LM
+
+# --- Use input arguments if exits
+if [ $# -ge 1 ] 
+then
+     while test $# -gt 0; do
+         case "$1" in
+             -m=*)
+                 model=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -e=*)
+                 expid=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             -v=*)
+                 version=$(echo $1|sed -e 's/^[^=]*=//g')
+                 shift
+                 ;;
+             * )
+                 echo "ERROR: option $1 not allowed."
+                 echo "*** EXITING THE SCRIPT"
+                 exit 1
+                 ;;
+         esac
+    done
+fi
+# --- 
+
+echo "--------------------"
+echo "EXPID: $expid       "
+echo "--------------------"
+
+echo "                    "
+echo "START CMOR...       "
+echo "$(date)             "
+echo "                    "
+
+if $login0
+then
+#----------------
+# part 1
+#----------------
+CaseName=NSSP585frc2_f19_tn14_speedup8_20241224
+real=11
+physics=5
+forcing=1
+init=1
+years1=(2020 $(seq 2030 10 2090) 2100)
+years2=(2029 $(seq 2039 10 2099) 2100)
+
+runcmor -c=$CaseName -m=$model -e=$expid -v=$version -r=$real -p=$physics -f=$forcing -i=$init -yrs1="${years1[*]}" -yrs2="${years2[*]}"
+#---
+fi
+#---
+
+wait
+echo "         "
+echo "CMOR DONE"
+echo "$(date)  "
+echo "~~~~~~~~~"
+
+# PrePARE QC check, create links and update sha256sum
+#${CMOR_ROOT}/workflow/cmorPost.sh -m=${model} -e=${expid} -v=${version} --verbose=false
