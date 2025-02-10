5d59692a0eaf8b49c0f64f9b568e37269e6eb742
Yanchun He <1725351+YanchunHe@users.noreply.github.com>
2025-02-10 22:50:36 +0100

    recipe for esm-ssp585-ocn-alk (#374)
    * descripiton of version v20230616
    
    * bug fix: use tabledir in case dir only for non-CMIP6 experiments
    
    * namelist for esm-ssp585-ocn-alk

--- namelists/CMIP6_NorESM2-LM/esm-ssp585-ocn-alk/checkcmorout.sh
+++ namelists/CMIP6_NorESM2-LM/esm-ssp585-ocn-alk/checkcmorout.sh
@@ -7,5 +7,5 @@ source $CMOR_ROOT/workflow/cmorParse.sh
 years1=(2015 $(seq 2020 10 2090) 2100)
 years2=(2019 $(seq 2029 10 2099) 2100)
 
-${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -o=$cmorout -yrs1="${years1[*]}" -yrs2="${years2[*]}"
+${CMOR_ROOT}/workflow/cmorCheck.sh -v=$version -e=$expid -m=$model -o=$cmoroutroot -yrs1="${years1[*]}" -yrs2="${years2[*]}"
 
